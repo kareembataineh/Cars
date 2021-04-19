828c359e09420183531c2fd0974f487ba832e8c3
kareembataineh <62024083+kareembataineh@users.noreply.github.com>
2021-04-19 16:22:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "CarSearch.h"
 
 using namespace std;
+using namespace std::chrono;
 
 int main() {
 
@@ -9,11 +10,34 @@ int main() {
     cars.printSize();
     cars.Search();
 
-    cars.heapSortLowToHigh();
-    cars.heapSortHighToLow();
-    cars.treeSortLowToHigh();
-    cars.treeSortHighToLow();
-    cars.printLinks(cars.getOutput());
+    //cars.treeSortLowToHigh();
+    //cars.treeSortHighToLow();
+
+    auto start = high_resolution_clock::now();
+    cars.heapSortLowToHigh(cars.getOutput());
+    auto end = high_resolution_clock::now();
+    auto time = duration_cast<milliseconds>(end-start);
+    cout << "Heapsort time: (low to high) " << time.count() << " ms" << endl;
+
+    start = high_resolution_clock::now();
+    cars.heapSortHighToLow(cars.getOutput());
+    end = high_resolution_clock::now();
+    time = duration_cast<milliseconds>(end-start);
+    cout << "Heapsort time: (high to low) " << time.count() << " ms" << endl;
+
+    start = high_resolution_clock::now();
+    cars.mergesortLowToHigh(cars.getOutput());
+    end = high_resolution_clock::now();
+    time = duration_cast<milliseconds>(end-start);
+    cout << "Mergesort time: (low to high) " << time.count() << " ms" << endl;
+
+    start = high_resolution_clock::now();
+    cars.mergesortHighToLow(cars.getOutput());
+    end = high_resolution_clock::now();
+    time = duration_cast<milliseconds>(end-start);
+    cout << "Mergesort time: (high to low) " << time.count() << " ms" << endl;
+
+    cars.printLinks(cars.mergesortLowToHigh(cars.getOutput()));
 
     return 0;
 }
